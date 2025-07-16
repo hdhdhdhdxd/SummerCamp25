0194442e2354fdbceed2759df2a9772cf51424c5
Achediez <hdhdhdhdxdyt@gmail.com>
2025-07-16 11:36:22 +0200

    Primer Hola Mundo

---
+++ HolaMundoSolucion/HolaMundoProyecto/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hola mundo!");
+        Console.WriteLine("===========");
+
+        Console.WriteLine(@"   /\                 /\");
+        Console.WriteLine(@"  / \'._   (\_/)   _.'/ \");
+        Console.WriteLine(@" /_.''._'--('.')--'_.''._\");
+        Console.WriteLine(@" | \_ / `;=/   \=;` \ _ / | ");
+        Console.WriteLine(@"  \/ `\__|`\___/`|__/`  \/");
+        Console.WriteLine(@"   `      \(/|\)/       `");
+        Console.WriteLine(@"           "" ` """);
+   }
+}
