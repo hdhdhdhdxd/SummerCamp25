3a1d0325a8968944219adb74793d75f42cad783a
Achediez <hdhdhdhdxdyt@gmail.com>
2025-07-17 10:32:24 +0200

    Ejercicio de solicitud de número para día de la semana

--- HolaMundoSolucion/HolaMundoProyecto/Program.cs
+++ HolaMundoSolucion/HolaMundoProyecto/Program.cs
@@ -1,8 +1,65 @@
-﻿internal class Program
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
 {
-    private static void Main(string[] args)
+
+
+    public static void Main()
     {
-        Console.WriteLine("Hola mundo!");
+        string nombreDia = "";
+
+        Console.WriteLine("Introduce el día de la semana");
+        var diaSemana = Console.ReadLine();
+
+        while (diaSemana != "0")
+        {
+            switch (diaSemana)
+            {
+                default:
+                    nombreDia = "Valor no válido";
+                    Console.WriteLine(nombreDia);
+                    break;
+                case "1":
+                    nombreDia = "Lunes";
+                    Console.WriteLine(nombreDia);
+                    break;
+                case "2":
+                    nombreDia = "Martes";
+                    Console.WriteLine(nombreDia);
+                    break;
+                case "3":
+                    nombreDia = "Miercoles";
+                    Console.WriteLine(nombreDia);
+                    break;
+                case "4":
+                    nombreDia = "Jueves";
+                    Console.WriteLine(nombreDia);
+                    break;
+                case "5":
+                    nombreDia = "Viernes";
+                    Console.WriteLine(nombreDia);
+                    break;
+                case "6":
+                    nombreDia = "Sabado";
+                    Console.WriteLine(nombreDia);
+                    break;
+                case "7":
+                    nombreDia = "Domingo";
+                    Console.WriteLine(nombreDia);
+                    break;
+            }
+            Console.WriteLine("Introduce el día de la semana");
+            diaSemana = Console.ReadLine();
+            if (diaSemana == "0")
+            {
+                nombreDia = "Saliendo, ¡Hasta luego!";
+                Console.WriteLine(nombreDia);
+            }
+        }
+
+
+
+        /*Console.WriteLine("Hola mundo!");
         Console.WriteLine("===========");
 
         Console.WriteLine(@"   /\                 /\");
@@ -12,5 +69,6 @@
         Console.WriteLine(@"  \/ `\__|`\___/`|__/`  \/");
         Console.WriteLine(@"   `      \(/|\)/       `");
         Console.WriteLine(@"           "" ` """);
-   }
+        */
+    }
 }
